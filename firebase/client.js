02705ece971b2596a5cd710de0fdb26a6883fb79
VECTORITCGROUP\apellejero <apellejero@ext.vectoritcgroup.com>
2022-02-01 15:43:14 +0100

    feature: (class 1) Sign in to GitHub with Firebase

---
+++ firebase/client.js
@@ -0,0 +1,43 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  GithubAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAETWcWQ2g72O-8WVAZmK_ukiaHyN-Tvwo',
+  authDomain: 'devter-c4868.firebaseapp.com',
+  projectId: 'devter-c4868',
+  storageBucket: 'devter-c4868.appspot.com',
+  messagingSenderId: '32923759936',
+  appId: '1:32923759936:web:6a7fa67eaee42955eb36a5',
+  measurementId: 'G-G9QZNS9FYH',
+};
+
+initializeApp(firebaseConfig);
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+  const { displayName, email, photoURL } = user;
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email,
+  };
+};
+
+export const _onAuthStateChanged = (onChange) => {
+  const auth = getAuth();
+  return onAuthStateChanged(auth, (user) => {
+    const normalizedUser = mapUserFromFirebaseAuthToUser(user);
+    onChange(normalizedUser);
+  });
+};
+
+export const loginWithGitHub = () => {
+  const githubProvider = new GithubAuthProvider();
+  const auth = getAuth();
+  console.log(githubProvider);
+  return signInWithPopup(auth, githubProvider);
+};
